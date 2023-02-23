907f99b2e3aa3891654c3fd48e24eb6cdda20744
Brilliant Makanjju <106150499+brilliantmakanju@users.noreply.github.com>
2023-02-23 01:56:49 +0100

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,15 @@
+import Head from "next/head"
+
 const About = () => {
 	return (
 		<>
+			<Head>
+				<title>Imdb-Clone | About </title>
+        			<meta content="width=device-width, initial-scale=1" name="viewport" />
+        			<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<h1 className="font-bold text-[40px] mx-[20px] text-amber-500  " >About </h1>
-			<div>
-				
-			</div>
+			
 		</>
 	)
 }
