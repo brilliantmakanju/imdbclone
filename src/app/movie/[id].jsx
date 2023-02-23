6d81ac64ab5d1e7521d9bb2a0865b3d206bcd5de
Brilliant Makanjju <106150499+brilliantmakanju@users.noreply.github.com>
2023-02-23 01:41:41 +0100

    Update [id].jsx

--- src/app/movie/[id].jsx
+++ src/app/movie/[id].jsx
@@ -1,6 +1,6 @@
 export default function MoviePage(params) {
 	const movieId = params.id
-	const res = await fetch(`http://127.0.0.1:8000/Movie/${movieId}`)
+	const res = await fetch(`https://pyapimovie.pythonanywhere.com/Movie/${movieId}`)
 	return (
 		<>
 			MoviePage
